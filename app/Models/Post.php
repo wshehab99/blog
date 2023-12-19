6981937cc207b43eaa3fa20b7cfbff7c6f5ee8bb
waleed <waleedbinshihab@gmail.com>
2023-12-19 11:47:44 +0300

    filter

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -21,14 +21,21 @@ class Post extends Model
     }
     public function scopeFilter($query,array $filters): void
     {
-        $query->when($filters['search']?? false, fn($query, $search)=>
-            $query
+        //search filter
+        $query->when($filters['search']?? false, fn($query, $search)=> $query
+            ->where(fn($query)=> $query
                 ->where('title','like','%'.$search.'%')
-                ->orWhere('body','like','%'.$search.'%'));
+                ->orWhere('body','like','%'.$search.'%'))
+        );
+        //category filter
         $query->when($filters['category']?? false, fn($query, $category)=>
             $query->whereHas('category', fn($query)=>
                     $query->where('id', $category)
                 ));
-
+        //author filter
+        $query->when($filters['author']?? false, fn($query, $author)=>
+            $query->whereHas('author', fn($query)=>
+                $query->where('username', $author)
+                ));
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,33 +1,10 @@
 <?php
 
 use App\Http\Controllers\PostController;
-use App\Models\User;
 use Illuminate\Support\Facades\Route;
-use App\Models\Post;
-use App\Models\Category;
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+
 
 Route::get('/', [PostController::class,'index']);
 Route::get('/post/{post:id}',[PostController::class,'show']);
-Route::get('/category/{category}',function (Category $category){
-    return view('posts',[
-        "posts"=>$category->posts,
-        "categories"=>Category::all(),
-        'currentCategory'=>$category,
-    ]);
-});
-Route::get('/users/{user:username}',function (User $user){
-    return view('category',[
-        'category'=>User::find($user->id)
-    ]);
-});
+
 
