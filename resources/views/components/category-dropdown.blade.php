054c1bacfbf9d33fe2117bbfa7f40913b4453a7c
waleed <waleedbinshihab@gmail.com>
2023-12-19 17:20:22 +0300

    pagination

--- resources/views/components/category-dropdown.blade.php
+++ resources/views/components/category-dropdown.blade.php
@@ -1,11 +1,11 @@
 <div class="relative flex lg:inline-flex items-center bg-gray-100 rounded-xl">
     <x-dropdown :currentCategory="$currentCategory??null">
-        <x-dropdown-item href="/">
+        <x-dropdown-item href="/?{{http_build_query(request()->except('category','page'))}}">
             All
         </x-dropdown-item>
         @foreach($categories as $category)
             <x-dropdown-item
-                href="/?category={{$category->id}}&{{http_build_query(request()->except('category'))}}"
+                href="/?category={{$category->id}}&{{http_build_query(request()->except('category','page'))}}"
                 :active="isset($currentCategory)&&$currentCategory->is($category)">
                 {{ucwords($category->name)}}
             </x-dropdown-item>
